9eb4ab89f23914544f5641cbbf067faf0b667c9c
4000D <dkdkajej@gmail.com>
2017-07-16 17:03:35 +0900

    stop...

--- MBLOKUS/main.cpp
+++ MBLOKUS/main.cpp
@@ -1,43 +1,41 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 #define EPS 1e-03
-#define MAX 202
+#define MAX 30
 
-int c, N, M;
-double d[MAX];
+struct Block {
+  std::vector<int> pts;
+};
 
-double test(double dist) {
-  int s = 0;
-  for (int i = 1; i < N; i++) {
-    int j = s + 1;
-    for (; j < M && d[j] - d[s] < dist; j++);
-    if (j == M) return false;
-    s = j;
-  }
-  return true;
-}
+struct Spot {
+  int x, y;
+  bool blocked;
+  bool possible;
+  Spot(){}
+  Spot(int _x, int _y, bool b) : x(_x), y(_y), blocked(b) {}
+};
+
+Spot d[MAX][MAX];
+int N, M;
 
 int main(int argc, char const *argv[]) {
+  int c;
+  char ch;
 
   std::cin >> c;
 
   while (c--) {
     std::cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-      std::cin >> d[i];
-    }
-
-    double low = 0, mid, high = d[M - 1] - d[0];
-    for (int i = 0; i < 100; i++) {
-      mid = (high + low) / 2;
-      if (test(mid)) {
-        low = mid;
-      } else {
-        high = mid;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        std::cin >> ch;
+        d[i][j].blocked = ch == '.' ? false : true;
+        d[i][j].possible = !d[i][j].blocked;
       }
     }
-    printf("%.2lf\n", mid);
+
   }
 
   return 0;
